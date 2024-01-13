dca1255d18adc4650e52433f8d71e096e125670e
Shaa test <shvyrev.anton78@gmail.com>
2024-01-13 20:50:39 +0500

    freqlogBranchCommit

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,44 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
+        for(int i = 0; i < 1000; i++) {
             Thread thread = new Thread(() -> {
                 int amountR = (int) Arrays.stream(generateRoute("RLRFR", 100).split(""))
                         .filter(x -> x.equals("R"))
                         .count();
                 System.out.println(amountR);
-                synchronized (sizeToFreq) {
-                    if (sizeToFreq.containsKey(amountR)) {
+                synchronized (sizeToFreq){
+                    if(sizeToFreq.containsKey(amountR)){
                         sizeToFreq.put(amountR, sizeToFreq.get(amountR) + 1);
-                    } else {
+                    }else {
                         sizeToFreq.put(amountR, 1);
                     }
+                    sizeToFreq.notify();
                 }
             });
-            thread.start();
             threads.add(thread);
         }
+        Thread printThread = new Thread(() -> {
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq){
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Max: " + sizeToFreq.keySet().stream().
+                            sorted().
+                            collect(Collectors.toList()).get(sizeToFreq.size() - 1));
+                }
+            }
+        });
+        printThread.start();
         for (Thread thread : threads) {
+            thread.start();
             thread.join();
         }
+        printThread.interrupt();
+
         int max = sizeToFreq.keySet().stream().sorted().collect(Collectors.toList()).get(sizeToFreq.size() - 1);
         System.out.printf("""
                 Самое частое количество повторений %d (встретилось %d раз)
@@ -33,6 +51,7 @@ public class Main {
                 """, max, sizeToFreq.get(max));
         sizeToFreq.keySet().forEach(x -> System.out.printf("- %d (%d раз)\n", x, sizeToFreq.get(x)));
     }
+
 
 
     public static String generateRoute(String letters, int length) {
